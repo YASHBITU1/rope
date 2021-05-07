ecafe4e719f5505e8cdce7620cefb04407f90c7b
YASHBITU1 <76907769+YASHBITU1@users.noreply.github.com>
2021-05-07 11:20:45 +0530

    Add files via upload

--- rope.js
+++ rope.js
@@ -1,31 +1,21 @@
 class rope {
-constructor(body1,body2,offsetX,offsetY){
-this.offsetX=offsetX
-this.offsetX=offsetX
-var options={
-    bodyA:body1,
-    bodyB:body2,
-    pointB{x:this.offsetX, y:this.offsetY}
-}
-//console.log(options);
-this.rope=Constraint.create(options)
-World.add(world,this.rope)
-}
-}
-display(){
- var pointA=this.rope.bodyA.position;
- var pointB=this.rope.bodyB.position;
+    constructor(body1,body2,x1,y1) {
+        var options = {
+          bodyA : body1,
+          bodyB : body2,
+          point:{x:x1,y:y1}
+        }
+        this.x1 = x1;
+        this.y1 = y1;
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+    }
 
- strokeWeight(2);
- var Anchor1X=pointA.x
- var Anchor1Y=pointA.y
-
- var Anchor2X=pointB.x+this.offsetX
- var Anchor2Y=pointB.y+this.offsetY
-
- line(Anchor1X,Anchor1Y,Anchor2X,Anchor2Y);
-
-
-}
-
-
+    display() {
+            var pointA = this.rope.bodyA.position;
+            var pointB = this.rope.bodyB.position;
+            strokeWeight(2);
+            line(pointA.x,pointA.y,pointB.x+this.x1,pointB.y+this.y1);
+            
+        } 
+    }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
-
+const Constraint = Matter.Constraint;
+const Render = Matter.Render;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
+var ball1,ball2,ball3,ball4,ball5;
+var rope1,rope2,rope3,rope4,rope5;
+var roof1;
+var engine;
+var world;
 function preload()
 {
 	
@@ -11,14 +16,26 @@ function preload()
 
 function setup() {
 	createCanvas(800, 700);
-
-
 	engine = Engine.create();
 	world = engine.world;
 
-	var rope1 = new rope(bobobject1.body,roofObject.body,-bobDiameter*2,0)
+	roof1 = new roof(200,200,200,25);
 
+    ball1 = new ball(120,300,40);
+    ball2 = new ball(160,300,40);
+    ball3 = new ball(200,300,40);
+    ball4 = new ball(240,300,40);
+    ball5 = new ball(280,300,40);
 
+	rope1 = new rope(ball1.body,roof1.body,-80,0);
+	rope2 = new rope(ball2.body,roof1.body,-40,0);
+  rope3 = new rope(ball3.body,roof1.body,0,0);
+  rope4 = new rope(ball4.body,roof1.body,40,0);
+  rope5 = new rope(ball5.body,roof1.body,80,0);
+
+  var render = Render.create({
+    element:document.body,engine:engine,options:{width:1200,height:700,wireFrames:false}
+  })
 	Engine.run(engine);
   
 }
@@ -28,9 +45,24 @@ function draw() {
   rectMode(CENTER);
   background(0);
   
+  roof1.display();
+  ball1.display();
+  ball2.display();
+  ball3.display();
+  ball4.display();
+  ball5.display();
+  rope1.display();
+  rope2.display();
+rope3.display();
+rope4.display();
+rope5.display();
   drawSprites();
  
 }
 
-
+function keyPressed(){
+  if (keyCode === UP_ARROW){
+    Matter.Body.applyForce(ball1.body,ball1.body.position,{x:-25,y:-30})
+  }
+}
 
